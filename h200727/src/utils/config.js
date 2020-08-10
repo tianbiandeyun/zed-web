3faf8aa2b4be38ea1662e5da422f8033cd142a0e
zed <158178152@qq.com>
2020-08-10 16:04:02 +0800

    test 开始

--- h200727/src/utils/config.js
+++ h200727/src/utils/config.js
@@ -25,6 +25,22 @@ export const REQUEST_URL = `${PROJECT_REQUEST}?r=interface/api&ac=hxdf&ic=${PROJ
 export const REQUEST_SHARE_URL = `${PROJECT_SHARE_REQUEST}/h/retobridge.html?`;
 
 /**
+ *接口名
+ * */
+export const PROJECT_INTERFACE = {
+    set_uv_statistics: 'set_uv_statistics', // uv
+    set_pv_statistics: 'set_pv_statistics', // pv
+    get_jsconf: 'get_jsconf', // 获取微信配置
+    set_video_click_count: 'set_video_click_count', // 设置视频点击次数
+    has_user_info: 'has_user_info', // 判断是否填写信息
+    send_phone_identifying_code: 'send_phone_identifying_code', // 发送短信
+    get_video_list: 'get_video_list', // 获取视频列表
+    set_user_info: 'set_user_info', // 设置用户信息
+    set_user_organization: 'set_user_organization', // 设置一二级组织
+    getopenid: 'getopenid' // 获取 openid
+};
+
+/**
  * 测试题数据
  * */
 export const TEST_QUESTION_LIST = [
@@ -299,19 +315,3 @@ export const TEST_QUESTION_LIST = [
         ]
     }
 ];
-
-/**
- *接口名
- * */
-export const PROJECT_INTERFACE = {
-    set_uv_statistics: 'set_uv_statistics', // uv
-    set_pv_statistics: 'set_pv_statistics', // pv
-    get_jsconf: 'get_jsconf', // 获取微信配置
-    set_video_click_count: 'set_video_click_count', // 设置视频点击次数
-    has_user_info: 'has_user_info', // 判断是否填写信息
-    send_phone_identifying_code: 'send_phone_identifying_code', // 发送短信
-    get_video_list: 'get_video_list', // 获取视频列表
-    set_user_info: 'set_user_info', // 设置用户信息
-    set_user_organization: 'set_user_organization', // 设置一二级组织
-    getopenid: 'getopenid' // 获取 openid
-};
