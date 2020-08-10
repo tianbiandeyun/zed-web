5bebfa71e3d3d6da76c46a3170451a8dfe14fe7c
zed <158178152@qq.com>
2020-08-10 16:18:35 +0800

    test 开始

--- h200727/src/views/question.vue
+++ h200727/src/views/question.vue
@@ -1,6 +1,16 @@
 <template>
     <section class="question-container">
 
+        <div class="top">
+            <img src="../assets/images/test-top.png" alt="">
+        </div>
+        <div class="right">
+            <img src="../assets/images/test-right.png" alt="">
+        </div>
+        <div class="left">
+            <img src="../assets/images/test-bottom.png" alt="">
+        </div>
+
     </section>
 </template>
 
@@ -8,16 +18,55 @@
     export default {
         name: "question",
         data() {
-            return {
-                test_list: []
-            }
+            return {}
         },
         mounted() {
-            this.$utils.setDocumentTitle('职业倾向测评')
+            this.$utils.setDocumentTitle('职业倾向测评');
+            console.log(this.$config.TEST_QUESTION_LIST)
         }
     }
 </script>
 
 <style lang="less" scoped>
+    .question-container {
+        height: 100%;
+        position: relative;
 
+        .top {
+            border: 1px solid black;
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            font-size: 0;
+
+            img {
+                width: 100%;
+            }
+        }
+
+        .right {
+            border: 1px solid black;
+            position: absolute;
+            bottom: 0;
+            right: 0;
+            font-size: 0;
+
+            img {
+                width: 100%;
+            }
+        }
+
+        .left {
+            border: 1px solid black;
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            font-size: 0;
+
+            img {
+                width: 100%;
+            }
+        }
+    }
 </style>
