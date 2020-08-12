2c3175af8cd8093b4dcf1af8e576216aa61a5d1f
cel <158178152@qq.com>
2020-08-12 13:08:38 +0800

    答题结果算出，添加接口

--- h200727/src/views/result.vue
+++ h200727/src/views/result.vue
@@ -14,30 +14,37 @@
             }
         },
         mounted(){
-            this.$utils.setDocumentTitle('答题结果');
+            const that = this;
 
-            this.$store.dispatch('_getQuestionResult', {
-                im: this.$config.PROJECT_INTERFACE.get_answer_record,
+            window.addEventListener("popstate", function(e) {
+                // that.$router.replace('/test');
+                alert('后退')
+            }, false);
+
+            that.$utils.setDocumentTitle('答题结果');
+
+            that.$store.dispatch('_getQuestionResult', {
+                im: that.$config.PROJECT_INTERFACE.get_answer_record,
                 fps: {
-                    open_id: this.getOpenid_info.back_value.open_id,
+                    open_id: that.getOpenid_info.back_value.open_id,
                 },
-                url: this.$config.REQUEST_URL
+                url: that.$config.REQUEST_URL
             }).then(res => {
 
                 let _result = +res.back_value;
 
                 if(_result <= 2){
-                    this.img_src = require('../assets/images/1.png')
+                    that.img_src = require('../assets/images/1.png')
                     return false;
                 }
 
                 if(2 < _result && _result <= 16){
-                    this.img_src = require('../assets/images/2.png')
+                    that.img_src = require('../assets/images/2.png')
                     return false;
                 }
 
                 if(16 < _result && _result <= 23){
-                    this.img_src = require('../assets/images/3.png')
+                    that.img_src = require('../assets/images/3.png')
                     return false;
                 }
 
