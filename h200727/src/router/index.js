c883f27a374469ed0d50f58c4499baabcc3a559e
cel <158178152@qq.com>
2020-08-12 09:43:33 +0800

    在家开发测试’

--- h200727/src/router/index.js
+++ h200727/src/router/index.js
@@ -7,7 +7,7 @@ const routes = [
     {
         path: '/',
         name: 'Home',
-        component: () => import('../views/home.vue')
+        component: () => import('../views/Home.vue')
     },
     {
         path: '/login',
