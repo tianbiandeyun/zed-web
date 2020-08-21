ca73b6a20dc1deccd19ffde18bbe128493a49993
zed <158178152@qq.com>
2020-08-21 19:25:09 +0800

    开始开发h200728

--- h200728/src/views/Home.vue
+++ h200728/src/views/Home.vue
@@ -5,17 +5,34 @@
             <img src="../assets/images/banner@3x.png" alt="">
         </header>
 
-        <p>{{nowMonth}}</p>
+        <!--日历盘-->
+        <div class="calender-box">
 
-        <div v-for="(item,index) in visibeDays" :key="index">
-            {{item.getDate()}}
+            <div class="calender">
+
+                <div class="calender_header">
+                    <p>已有青创币 9999 枚</p>
+                    <p>幸运抽牌 ></p>
+                </div>
+
+                <div class="calender_main">
+                    <div class="calender_main__item"
+                         :class="{pro_month_active: item.getMonth()+1 !== date.getMonth()+1}"
+                         v-for="(item,index) in visibeDays"
+                         :key="index">
+                        {{item.getDate()}}
+                    </div>
+                </div>
+
+            </div>
+
         </div>
+
 
     </section>
 </template>
 
 <script>
-
     export default {
         name: 'Home',
         components: {},
@@ -69,9 +86,61 @@
 
         .banner {
             width: 100%;
+            font-size: 0;
 
             img {
                 width: 100%;
+            }
+        }
+
+        .calender-box {
+            border: 1px solid black;
+
+            .calender {
+                margin: 0 auto;
+                width: 640px;
+                border: 1px solid red;
+                -webkit-border-radius: @default-element-border-radius;
+                -moz-border-radius: @default-element-border-radius;
+                border-radius: @default-element-border-radius;
+                overflow: hidden;
+
+                .calender_header {
+                    background-color: @default-app-color-primary;
+                    padding: 20px;
+                    display: grid;
+                    grid-template-columns: repeat(2, 1fr);
+                    color: #fff;
+                    font-size: @default-font-size-30;
+
+                    p {
+                        &:nth-of-type(2) {
+                            text-align: right;
+                        }
+                    }
+
+                }
+
+                .calender_main {
+                    display: grid;
+                    grid-template-columns: repeat(7, 1fr);
+                    grid-template-rows: repeat(6, 80px);
+                    justify-items: center;
+                    align-items: center;
+                    font-size: @default-font-size-30;
+                    color: @default-font-color-content;
+
+                    .calender_main__item {
+
+                    }
+
+                    /*不是本月则显示灰色*/
+                    .pro_month_active {
+                        color: @default-font-color-sub;
+                    }
+
+                }
+
             }
         }
     }
