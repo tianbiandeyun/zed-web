545d9fab4163e1aace7be8018a7098d402d15111
zed <158178152@qq.com>
2020-08-21 11:41:04 +0800

    配置 h200728

--- h200728/src/utils/utils.js
+++ h200728/src/utils/utils.js
@@ -63,5 +63,14 @@ export default {
         } else {
             return false;
         }
-    }
+    },
+    /**
+     * 获取地址栏参数
+     * */
+    getUrlParam(name) {
+        let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); // 构造一个含有目标参数的正则表达式对象
+        let r = window.location.search.substr(1).match(reg); // 匹配目标参数
+        if (r != null) return unescape(r[2]);
+        return null; // 返回参数值
+    };
 }
