b553953a7b279d73db08fc4116c59ca613b774f6
zed <158178152@qq.com>
2020-08-22 19:33:34 +0800

    calender生成组件

--- h200728/src/views/Home.vue
+++ h200728/src/views/Home.vue
@@ -6,37 +6,17 @@
         </header>
 
         <!--日历盘-->
-        <div class="calender-box">
-
-            <div class="calender">
-
-                <div class="calender_header">
-                    <p>已有青创币 9999 枚</p>
-                    <p>幸运抽牌 ></p>
-                </div>
-
-                <div class="calender_main">
-                    <div class="calender_main__item"
-                         :class="{pro_month_active: item.getMonth()+1 !== date.getMonth()+1}"
-                         v-for="(item,index) in visibeDays"
-                         :key="index">
-                        {{item.getDate()}}
-                    </div>
-                    <div class="calender_main__nowMonth">{{nowMonth}}月</div>
-                </div>
-
-            </div>
-
-        </div>
-
+        <calender :date="date"></calender>
 
     </section>
 </template>
 
 <script>
+    import calender from '../components/calender/calender'
+
     export default {
         name: 'Home',
-        components: {},
+        components: {calender},
         data() {
             return {
                 date: new Date()
@@ -45,39 +25,7 @@
         mounted() {
 
         },
-        methods: {},
-        computed: {
-            /**
-             * 把当前月格式化42天
-             * */
-            visibeDays() {
-
-                // 获取当前日期
-                let {year, month} = this.$Utils.getYearMonthDay(this.date);
-
-                // 获取当前月的第一天
-                let currentFirstDay = this.$Utils.getDate(year, month, 1);
-
-                // 当月第一天是周几
-                let week = currentFirstDay.getDay();
-
-                // 本日历盘开始的第一天
-                let start = currentFirstDay - week * 60 * 60 * 1000 * 24;
-
-                let arr = [];
-
-                for (let i = 0; i < 42; i++) {
-                    arr.push(new Date(start + i * 60 * 60 * 1000 * 24))
-                }
-
-                return arr;
-
-            },
-            nowMonth() {
-                let {month} = this.$Utils.getYearMonthDay(this.date);
-                return month + 1
-            }
-        }
+        methods: {}
     }
 </script>
 
@@ -94,74 +42,6 @@
 
             img {
                 width: 100%;
-            }
-        }
-
-        .calender-box {
-
-            .calender {
-                background-color: #fff;
-                margin: 0 auto;
-                width: 680px;
-                -webkit-border-radius: @default-element-border-radius;
-                -moz-border-radius: @default-element-border-radius;
-                border-radius: @default-element-border-radius;
-                overflow: hidden;
-
-                .calender_header {
-                    background-color: @default-app-color-primary;
-                    padding: 20px;
-                    display: grid;
-                    grid-template-columns: repeat(2, 1fr);
-                    color: #fff;
-                    font-size: @default-font-size-30;
-
-                    p {
-                        &:nth-of-type(2) {
-                            text-align: right;
-                        }
-                    }
-
-                }
-
-                .calender_main {
-                    display: grid;
-                    grid-template-columns: repeat(7, 1fr);
-                    grid-template-rows: repeat(6, 80px);
-                    justify-items: center;
-                    align-items: center;
-                    font-size: @default-font-size-30;
-                    font-weight: 600;
-                    color: @default-font-color-content;
-                    padding-bottom: 60px;
-                    position: relative;
-
-                    .calender_main__item {
-                        z-index: 9;
-                    }
-
-                    .calender_main__nowMonth {
-                        width: 400px;
-                        height: 280px;
-                        text-align: center;
-                        line-height: 280px;
-                        position: absolute;
-                        top: 50%;
-                        left: 50%;
-                        margin-left: -200px;
-                        margin-top: -180px;
-                        z-index: 0;
-                        color: @default-app-color-background;
-                        font-size: 240px;
-                    }
-
-                    /*不是本月则显示灰色*/
-                    .pro_month_active {
-                        color: @default-font-color-sub;
-                    }
-
-                }
-
             }
         }
     }
