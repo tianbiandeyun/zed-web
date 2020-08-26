24374d4ad5c1cde335271c7d71b3dad35f3561ac
zed <158178152@qq.com>
2020-08-26 13:58:06 +0800

    .

--- h200728/src/store/index.js
+++ h200728/src/store/index.js
@@ -1,12 +1,34 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import utils from '../utils/utils'
+import axios from 'axios'
+
+axios.defaults.withCredentials = true;
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    state: {},
-    mutations: {},
-    actions: {},
-    modules: {},
-    getters: {}
+    state: {
+        openid_info: ''
+    },
+    mutations: {
+        getOpenid(state, res) {
+            state.openid_info = res;
+        },
+    },
+    actions: {
+        getRequest({commit}, params) {
+            const [im, fps = {}, url] = [params.im, params.fps, params.url];
+            const requestUrl = utils.produceRequestUrl(im, fps, url);
+            return new Promise((resolve, reject) => {
+                axios.post(requestUrl).then(res => {
+                    commit('getOpenid', res.data);
+                    resolve(res.data);
+                });
+            });
+        }
+    },
+    getters: {
+        openid_info: state => state.openid_info,
+    }
 })
--- h200728/src/utils/config.js
+++ h200728/src/utils/config.js
@@ -31,5 +31,7 @@ export const REQUEST_SHARE_URL = `${PROJECT_SHARE_REQUEST}/h/retobridge.html?`;
 /**
  *接口名
  * */
-export const PROJECT_INTERFACE = {};
+export const PROJECT_INTERFACE = {
+    getopenid: 'getopenid'
+};
 
