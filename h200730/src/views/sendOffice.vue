8c95752bd51e73297f11be2d30b5b9c9621828c5
zed <158178152@qq.com>
2020-09-10 09:49:39 +0800

    完结

--- h200730/src/views/sendOffice.vue
+++ h200730/src/views/sendOffice.vue
@@ -1,17 +1,56 @@
 <template>
     <section class="send-office-container">
-        <OfficItem>
-            <p>发布日期：2020-0-13</p>
-        </OfficItem>
+
+        <div v-if="office_list.length !== 0">
+            <OfficItem v-for="(item,index) in office_list" :key="index" :office="item">
+                <p>{{item.recruitment_unit}}</p>
+            </OfficItem>
+        </div>
+        <div class="air" v-else>
+            暂时没投递任何职位
+        </div>
+
     </section>
 </template>
 
 <script>
     import OfficItem from '../components/officeItem/office_item'
+    import {mapGetters} from 'vuex'
 
     export default {
         name: "sendOffice",
-        components: {OfficItem}
+        components: {OfficItem},
+        data() {
+            return {
+                office_list: []
+            }
+        },
+        mounted() {
+
+            this.$Toast.loading({
+                message: '加载中...',
+                forbidClick: true,
+                duration: 0,
+                overlay: true
+            });
+
+            this.$store.dispatch('fetchData', {
+                im: this.$Config.PROJECT_INTERFACE.get_application_record,
+                fps: {
+                    open_id: this.openid_info.back_value.open_id
+                },
+                url: this.$Config.REQUEST_URL
+            }).then(res => {
+                this.$Toast.clear();
+                console.log(res.back_value);
+                this.office_list = res.back_value;
+            });
+        },
+        computed: {
+            ...mapGetters([
+                'openid_info'
+            ])
+        }
     }
 </script>
 
@@ -20,6 +59,13 @@
         height: 100%;
         overflow-y: scroll;
         background-color: @default-app-color-background;
+
+        .air {
+            text-align: center;
+            font-size: @default-font-size-46;
+            color: @default-font-color-sub;
+            line-height: 2;
+        }
     }
 
 </style>
