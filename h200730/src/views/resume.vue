b84e396ce2dbaf9bee797deed409da330970d6db
zed <158178152@qq.com>
2020-09-10 10:57:23 +0800

    完结

--- h200730/src/views/resume.vue
+++ h200730/src/views/resume.vue
@@ -11,31 +11,31 @@
         name: "resume",
         mounted() {
 
-            console.log(this.resume_info.back_value);
-
             let _resume = {
-                '联系方式': {
-                    name: '',
-                    sex: '',
-                    mail: '',
-                    phone: ''
+                'contact': {
+                    name: this.resume_info.back_value.name,
+                    sex: this.resume_info.back_value.sex,
+                    mail: this.resume_info.back_value.mail,
+                    phone: this.resume_info.back_value.phone
                 },
-                '年龄及专业': {
-                    native_place: '',
-                    address: '',
-                    date_of_birth: '',
-                    graduate_institutions: '',
-                    specialty: '',
-                    education_level: ''
+                'age': {
+                    native_place: this.resume_info.back_value.native_place,
+                    address: this.resume_info.back_value.address,
+                    date_of_birth: this.resume_info.back_value.date_of_birth,
+                    graduate_institutions: this.resume_info.back_value.graduate_institutions,
+                    specialty: this.resume_info.back_value.specialty,
+                    education_level: this.resume_info.back_value.education_level
                 },
-                '岗位相关': {
-                    political_status: '',
-                    expected_position: '',
-                    self_introduction: '',
-                    qualification_certificate: ''
+                'office': {
+                    political_status: this.resume_info.back_value.political_status,
+                    expected_position: this.resume_info.back_value.expected_position,
+                    self_introduction: this.resume_info.back_value.self_introduction,
+                    qualification_certificate: this.resume_info.back_value.qualification_certificate
                 },
-                '工作经历': []
-            }
+                'exper': this.resume_info.back_value.work_history_list
+            };
+
+            console.log(_resume);
 
         },
         computed: {
