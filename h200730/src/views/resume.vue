1e1f3ff42488de2605c758b8747c21fe5a82d455
zed <158178152@qq.com>
2020-09-10 14:21:12 +0800

    完结

--- h200730/src/views/resume.vue
+++ h200730/src/views/resume.vue
@@ -3,11 +3,23 @@
 
         <div v-for="(item,index) in resume" :key="index">
 
-            <h2>{{item.title}}</h2>
+            <div class="resume-title">
+                <h2>{{item.title}}</h2>
+            </div>
 
-            <p v-for="(key ,value) in item.content" :key="value">
-                {{value}}：{{key}}
-            </p>
+            <div class="resume" v-for="(key ,value) in item.content" :key="value">
+
+                <div v-if="typeof key === 'object'" class="resume-exper">
+                    <p v-for="(k ,v) in key" :key="v">
+                        {{v}}：<span>{{k}}</span>
+                    </p>
+                </div>
+
+                <div v-else class="resume-content">
+                    {{value}}：<span>{{key}}</span>
+                </div>
+
+            </div>
 
         </div>
 
@@ -30,6 +42,18 @@
         methods: {
             _setResume() {
                 let result = this.resume_info.back_value;
+                let work_list = [];
+
+                for (let i = 0; i < result.work_history_list.length; i++) {
+                    work_list.push({
+                        '工作单位': result.work_history_list[i].work_unit,
+                        '职位名称': result.work_history_list[i].name_of_post,
+                        '就职日期': result.work_history_list[i].start_date,
+                        '所属行业': result.work_history_list[i].industry,
+                        '工作描述': result.work_history_list[i].describe
+                    })
+                }
+
                 return [
                     {
                         'title': '联系方式',
@@ -61,7 +85,7 @@
                     },
                     {
                         'title': '工作经历',
-                        // 'content': result.work_history_list
+                        'content': work_list
                     }
                 ];
             }
@@ -76,5 +100,40 @@
 </script>
 
 <style lang="less" scoped>
+    .resume-container {
+        padding: 10px 0;
 
+        .resume-title {
+
+            h2 {
+                text-align: right;
+                background-color: @default-app-color-primary;
+                width: 200px;
+                padding: 10px 10px 10px 0;
+                color: #fff;
+                font-size: @default-font-size-34;
+                margin-bottom: 20px;
+                margin-top: 20px;
+            }
+        }
+
+        .resume {
+
+            .resume-exper, .resume-content {
+                line-height: 1.6;
+                padding-left: 60px;
+                font-size: @default-font-size-30;
+                color: @default-font-color-content;
+
+                span {
+                    color: @default-font-color-sub;
+                }
+            }
+
+            .resume-exper {
+                border: 1px solid red;
+                margin-bottom: 20px;
+            }
+        }
+    }
 </style>
