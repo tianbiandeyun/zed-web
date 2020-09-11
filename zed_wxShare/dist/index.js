b65a6d04aa44119cb5a785660e2b1879575a9419
zed <158178152@qq.com>
2020-09-11 12:02:12 +0800

    跟新微信分享

--- zed_wxShare/dist/index.js
+++ zed_wxShare/dist/index.js
@@ -16,49 +16,57 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
 var wxShare = function () {
     function wxShare(wxConfig, shareInfo) {
+        var debug = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : false;
+
         _classCallCheck(this, wxShare);
 
         this.wxConfig = wxConfig;
         this.shareInfo = shareInfo;
+        this.debug = debug;
     }
 
     _createClass(wxShare, [{
         key: 'init',
-        value: async function init() {
+        value: function init() {
             var that = this;
 
             _weixinJsSdk2.default.config({
-                debug: false,
+                debug: that.debug,
                 appId: that.wxConfig.appId, // 必填，公众号的唯一标识
                 timestamp: that.wxConfig.timestamp, // 必填，生成签名的时间戳
                 nonceStr: that.wxConfig.nonceStr, // 必填，生成签名的随机串
                 signature: that.wxConfig.signature, // 必填，签名，见附录1
-                jsApiList: JSON.parse(that.wxConfig.jsApiList_json) // 必填，需要使用的 JS 接口列表，所有JS接口列表见附录2
+                jsApiList: ['updateAppMessageShareData', 'updateTimelineShareData']
             });
 
             _weixinJsSdk2.default.ready(function () {
                 /**
                  * 分享给朋友
                  * */
-                _weixinJsSdk2.default.onMenuShareAppMessage({
+                _weixinJsSdk2.default.updateAppMessageShareData({
                     title: that.shareInfo.title, // 分享标题
                     desc: that.shareInfo.details, // 分享描述
                     link: that.shareInfo.link, // 分享链接
                     imgUrl: that.shareInfo.image, // 分享图标
                     success: function success() {
-                        console.log('分享成功');
+                        // window.alert('分享给朋友-设置成功')
+                    },
+                    fail: function fail() {
+                        window.alert('分享给朋友-设置失败');
                     }
                 });
                 /**
                  * 分享到朋友圈
                  * */
-                _weixinJsSdk2.default.onMenuShareTimeline({
+                _weixinJsSdk2.default.updateTimelineShareData({
                     title: that.shareInfo.title, // 分享标题
-                    desc: that.shareInfo.details, // 分享描述
-                    link: that.shareInfo.link, // 分享链接
+                    link: that.shareInfo.link, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
                     imgUrl: that.shareInfo.image, // 分享图标
                     success: function success() {
-                        console.log('分享成功');
+                        // window.alert('分享到朋友圈-设置成功')
+                    },
+                    fail: function fail() {
+                        window.alert('分享到朋友圈-设置失败');
                     }
                 });
             });
--- zed_wxShare/index.js
+++ zed_wxShare/index.js
@@ -1,51 +1,59 @@
 import wx from 'weixin-js-sdk'
 
 export default class wxShare {
-    constructor(wxConfig, shareInfo) {
+    constructor(wxConfig, shareInfo, debug = false) {
         this.wxConfig = wxConfig;
         this.shareInfo = shareInfo;
+        this.debug = debug;
     }
 
-    async init() {
+    init() {
         const that = this;
 
         wx.config({
-            debug: false,
+            debug: that.debug,
             appId: that.wxConfig.appId, // 必填，公众号的唯一标识
             timestamp: that.wxConfig.timestamp, // 必填，生成签名的时间戳
             nonceStr: that.wxConfig.nonceStr, // 必填，生成签名的随机串
             signature: that.wxConfig.signature, // 必填，签名，见附录1
-            jsApiList: JSON.parse(that.wxConfig.jsApiList_json), // 必填，需要使用的 JS 接口列表，所有JS接口列表见附录2
+            jsApiList: [
+                'updateAppMessageShareData',
+                'updateTimelineShareData',
+            ],
         });
 
-        wx.ready(function () {
+        wx.ready(() => {
             /**
              * 分享给朋友
              * */
-            wx.onMenuShareAppMessage({
+            wx.updateAppMessageShareData({
                 title: that.shareInfo.title, // 分享标题
                 desc: that.shareInfo.details, // 分享描述
                 link: that.shareInfo.link, // 分享链接
                 imgUrl: that.shareInfo.image, // 分享图标
                 success() {
-                    console.log('分享成功')
+                    // window.alert('分享给朋友-设置成功')
+                },
+                fail() {
+                    window.alert('分享给朋友-设置失败')
                 }
             });
             /**
              * 分享到朋友圈
              * */
-            wx.onMenuShareTimeline({
+            wx.updateTimelineShareData({
                 title: that.shareInfo.title, // 分享标题
-                desc: that.shareInfo.details, // 分享描述
-                link: that.shareInfo.link, // 分享链接
+                link: that.shareInfo.link, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
                 imgUrl: that.shareInfo.image, // 分享图标
                 success() {
-                    console.log('分享成功')
+                    // window.alert('分享到朋友圈-设置成功')
+                },
+                fail() {
+                    window.alert('分享到朋友圈-设置失败')
                 }
             });
         });
 
     }
-
 
 }
