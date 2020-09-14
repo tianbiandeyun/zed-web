1b4a0e74d054a1fbb4679448e135fb0cccbd3b82
zed <158178152@qq.com>
2020-09-14 11:48:49 +0800

    工作经历修改

--- h200730/src/views/officeDetails.vue
+++ h200730/src/views/officeDetails.vue
@@ -1,14 +1,38 @@
 <template>
     <section class="office-details-container">
 
+        <OfficItem :office="office_details">
+            <p class="position">发布时间：{{office_details.issuance_date}}</p>
+        </OfficItem>
+
+        <div>
+            <h2>职位描述：</h2>
+            <p>{{office_details.job_describe }}</p>
+        </div>
+        <div>
+            <h2>技能要求：</h2>
+            <p>{{office_details.skill_requirements }}</p>
+        </div>
+        <div>
+            <h2>岗位职责：</h2>
+            <p>{{office_details.describe }}</p>
+        </div>
+
     </section>
 </template>
 
 <script>
+    import OfficItem from '../components/officeItem/office_item'
     import {mapGetters} from 'vuex'
 
     export default {
         name: "officeDetails",
+        components: {OfficItem},
+        data() {
+            return {
+                office_details: {}
+            }
+        },
         mounted() {
             this.$store.dispatch('fetchData', {
                 im: this.$Config.PROJECT_INTERFACE.get_job_info,
@@ -18,7 +42,8 @@
                 },
                 url: this.$Config.REQUEST_URL
             }).then(res => {
-                console.log(res);
+                console.log(res.back_value)
+                this.office_details = res.back_value;
             });
         },
         computed: {
