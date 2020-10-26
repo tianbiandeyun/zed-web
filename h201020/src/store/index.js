4c0ea8e8d59634e7fca54e9da3f001d5962c326f
zed <158178152@qq.com>
2020-10-26 16:38:32 +0800

    add h201020

--- h201020/src/store/index.js
+++ h201020/src/store/index.js
@@ -8,45 +8,9 @@ axios.defaults.withCredentials = true;
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    state: {
-        openid_info: '',
-        user_info:''
-    },
-    mutations: {
-        getOpenid(state, res) {
-            state.openid_info = res;
-        },
-        getUserInfo(state, res) {
-            state.user_info = res;
-        }
-    },
+    state: {},
+    mutations: {},
     actions: {
-        /**
-         * 共享数据请求
-         * 添加 commit
-         * */
-        // 获取openid
-        getOpenid({commit}, params) {
-            const [im, fps = {}, url] = [params.im, params.fps, params.url];
-            const requestUrl = utils.produceRequestUrl(im, fps, url);
-            return new Promise((resolve, reject) => {
-                axios.get(requestUrl).then(res => {
-                    commit('getOpenid', res.data);
-                    resolve(res.data);
-                });
-            });
-        },
-        // 获取用户信息
-        getUserInfo({commit}, params) {
-            const [im, fps = {}, url] = [params.im, params.fps, params.url];
-            const requestUrl = utils.produceRequestUrl(im, fps, url);
-            return new Promise((resolve, reject) => {
-                axios.get(requestUrl).then(res => {
-                    commit('getUserInfo', res.data);
-                    resolve(res.data);
-                });
-            });
-        },
         /**
          * 通用请求：fetchDate
          * 数据不共享，单独局部使用
@@ -61,8 +25,5 @@ export default new Vuex.Store({
             });
         }
     },
-    getters: {
-        openid_info: state => state.openid_info,
-        user_info: state => state.user_info
-    }
+    getters: {}
 })
