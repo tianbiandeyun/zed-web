46840d633232621dba00a418192031747605cd63
zed <158178152@qq.com>
2021-03-30 16:48:18 +0800

    add Lkeep

--- Lkeep/src/pages/index/index.vue
+++ Lkeep/src/pages/index/index.vue
@@ -1,10 +1,15 @@
 <template>
   <section class="index-container">
 
-
-    <div class="record-box" v-for="(item,index) in 10" :key="index">
-      <ZedRecordItem></ZedRecordItem>
+    <div class="record-box" v-for="(item,index) in list" :key="index">
+      <v-sticky>
+        <h1 class="title">{{item.month}}月</h1>
+      </v-sticky>
+      <div class="item" v-for="(i,k) in item.list" :key="k">
+        <ZedRecordItem></ZedRecordItem>
+      </div>
     </div>
+
 
   </section>
 </template>
@@ -17,7 +22,16 @@
     components: { ZedRecordItem, echart },
     data() {
       return {
-        t: [80, 97, 99, 104, 111, 101, 87]
+        list: [
+          {
+            month: 3,
+            list: [1, 2, 4, 5, 6, 7, 8, 9]
+          },
+          {
+            month: 4,
+            list: [1, 2, 4, 5, 6, 7, 8, 9]
+          }
+        ]
       };
     },
     methods: {}
@@ -29,11 +43,25 @@
     padding: 20px;
 
     .record-box {
-      margin-bottom: 20px;
 
-      &:last-child {
-        margin-bottom: 0;
+      .title {
+        padding-left: 20px;
+        font-size: 20px;
+        font-weight: bold;
+        color: #1c2438;
+        background-color: #fff;
+        line-height: 2;
+        margin-bottom: 10px;
       }
+
+      .item {
+        margin-bottom: 20px;
+
+        &:last-child {
+          margin-bottom: 0;
+        }
+      }
+
     }
   }
 </style>
