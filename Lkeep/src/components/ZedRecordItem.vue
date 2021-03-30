d0e947e3fc9c48564a64cf53b82a5f384d79ea55
zed <158178152@qq.com>
2021-03-30 16:33:33 +0800

    add Lkeep

---
+++ Lkeep/src/components/ZedRecordItem.vue
@@ -0,0 +1,79 @@
+<template>
+  <section class="record-container">
+
+    <div class="header">
+      <div>
+        <p>2020年03月30日，星期二</p>
+        <p>上午</p>
+      </div>
+      <div>
+        <v-icon name="arrow" size="26px" color="#495060"></v-icon>
+      </div>
+    </div>
+    <v-divider></v-divider>
+    <div class="content">
+      <p>一周体重变化一周体重变化一周体重变化一周体重变化</p>
+    </div>
+
+  </section>
+</template>
+
+<script>
+  export default {
+    name: "ZedRecordItem"
+  };
+</script>
+
+<style lang="less" scoped>
+  .record-container {
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+    padding: 10px;
+    -webkit-border-radius: 4px;
+    -moz-border-radius: 4px;
+    border-radius: 4px;
+    -moz-box-shadow: 0 0 10px #dddee1;
+    -webkit-box-shadow: 0 0 10px #dddee1;
+    box-shadow: 0 0 10px #dddee1;
+
+    .header {
+      display: grid;
+      grid-template-columns: 9fr 1fr;
+      align-items: center;
+
+      div:nth-of-type(1) {
+
+        p:nth-of-type(1) {
+          color: #1c2438;
+          font-size: 16px;
+        }
+
+        p:nth-of-type(2) {
+          color: #495060;
+          font-size: 14px;
+        }
+      }
+
+      div:nth-of-type(2) {
+        display: flex;
+        align-items: center;
+        justify-items: center;
+      }
+    }
+
+    .content {
+
+      p {
+        color: #495060;
+        font-size: 14px;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-box-orient: vertical;
+        -webkit-line-clamp: 1;
+        word-break: break-all; /*追加这一行代码*/
+      }
+    }
+  }
+</style>
--- Lkeep/src/pages/index/index.vue
+++ Lkeep/src/pages/index/index.vue
@@ -1,52 +1,20 @@
 <template>
   <section class="index-container">
 
-    <div class="record">
 
-      <div class="header">
-        <div>
-          <p>2020年03月30日，星期二</p>
-          <p>上午</p>
-        </div>
-        <div>
-          <v-icon name="arrow" size="26px" color="#495060"></v-icon>
-        </div>
-      </div>
-      <v-divider></v-divider>
-      <div class="content">
-        <p>一周体重变化</p>
-        <echart :t="t" id="1"></echart>
-      </div>
-
-    </div>
-
-    <div class="record">
-
-      <div class="header">
-        <div>
-          <p>2020年03月29日，星期一</p>
-          <p>上午</p>
-        </div>
-        <div>
-          <v-icon name="arrow" size="26px" color="#495060"></v-icon>
-        </div>
-      </div>
-      <v-divider></v-divider>
-      <div class="content">
-        <p>一周体重变化</p>
-        <echart :t="t" id="2"></echart>
-      </div>
-
+    <div class="record-box" v-for="(item,index) in 10" :key="index">
+      <ZedRecordItem></ZedRecordItem>
     </div>
 
   </section>
 </template>
 
 <script>
+  import ZedRecordItem from "../../components/ZedRecordItem";
   import echart from "../../components/echart";
 
   export default {
-    components: { echart },
+    components: { ZedRecordItem, echart },
     data() {
       return {
         t: [80, 97, 99, 104, 111, 101, 87]
@@ -60,50 +28,11 @@
   .index-container {
     padding: 20px;
 
-    .record {
-      -webkit-box-sizing: border-box;
-      -moz-box-sizing: border-box;
-      box-sizing: border-box;
-      padding: 10px;
-      -webkit-border-radius: 4px;
-      -moz-border-radius: 4px;
-      border-radius: 4px;
-      -moz-box-shadow: 0 0 10px #dddee1;
-      -webkit-box-shadow: 0 0 10px #dddee1;
-      box-shadow: 0 0 10px #dddee1;
+    .record-box {
+      margin-bottom: 20px;
 
-      .header {
-        display: grid;
-        grid-template-columns: 9fr 1fr;
-        align-items: center;
-
-        div:nth-of-type(1) {
-
-          p:nth-of-type(1) {
-            color: #1c2438;
-            font-size: 16px;
-          }
-
-          p:nth-of-type(2) {
-            color: #495060;
-            font-size: 14px;
-          }
-        }
-
-        div:nth-of-type(2) {
-          display: flex;
-          align-items: center;
-          justify-items: center;
-        }
-      }
-
-      .content {
-
-        p {
-          text-align: center;
-          color: #495060;
-          font-size: 14px;
-        }
+      &:last-child {
+        margin-bottom: 0;
       }
     }
   }
