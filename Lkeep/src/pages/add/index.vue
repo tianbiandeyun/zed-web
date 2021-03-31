c0111c3f3c2facb38d463d1c2564baea4da68918
zed <158178152@qq.com>
2021-03-31 15:37:00 +0800

    add Lkeep

--- Lkeep/src/pages/add/index.vue
+++ Lkeep/src/pages/add/index.vue
@@ -8,39 +8,106 @@
 
     <div class="add-box">
       <div class="item">
+        <p>性别：</p>
+        <div>
+          <picker @change="selectSex" :value="sex" :range="a0">
+            {{a0[sex]}}
+          </picker>
+        </div>
+      </div>
+      <div class="item">
+        <p>身高：</p>
+        <div>
+          <input type="number" v-model="shenggao" placeholder="请输入身高">
+        </div>
+      </div>
+      <div class="item">
         <p>当前体重：</p>
         <div>
-          <input type="text" placeholder="请输入当前体重">
+          <input type="number" v-model="tizhong" placeholder="请输入当前体重">
+        </div>
+      </div>
+      <div class="item">
+        <p>BMI：</p>
+        <div>
+          <p>24.1</p>
+        </div>
+      </div>
+      <div class="item">
+        <p>今日目标：</p>
+        <div>
+          <input type="number" v-model="mubiao" placeholder="请输入今日目标">
         </div>
       </div>
       <div class="item">
         <p>时间段：</p>
         <div>
-          <picker @change="bindPickerChange" :value="index" :range="array">
-            <p>
-              当前选择：{{array[index]}}
-            </p>
+          <picker @change="selectTime" :value="time" :range="a1">
+            {{a1[time]}}
           </picker>
         </div>
       </div>
+      <div class="item">
+        <p>用餐情况：</p>
+        <div>
+          <picker @change="selectEat" :value="eat" :range="a2">
+            {{a2[eat]}}
+          </picker>
+        </div>
+      </div>
+      <div class="item">
+        <p>身体症状：</p>
+        <div>
+          <picker @change="selectDisease" :value="disease" :range="a3">
+            {{a3[disease]}}
+          </picker>
+        </div>
+      </div>
+      <div class="item">
+        <p>情绪状况：</p>
+        <div>
+          <picker @change="selectMoody" :value="moody" :range="a4">
+            {{a4[moody]}}
+          </picker>
+        </div>
+      </div>
+      <div class="item" v-if="sex == 2">
+        <p>生理期：</p>
+        <div>
+          <picker @change="selectTo" :value="to" :range="a5">
+            {{a5[to]}}
+          </picker>
+        </div>
+      </div>
+      <div class="item">
+        <p>睡眠状态：</p>
+        <div>
+          <picker @change="selectSleep" :value="sleep" :range="a6">
+            {{a6[sleep]}}
+          </picker>
+        </div>
+      </div>
+      <div class="item">
+        <p>健身状态：</p>
+        <div>
+          <picker @change="selectDuanLian" :value="duanlian" :range="a7">
+            {{a7[duanlian]}}
+          </picker>
+        </div>
+      </div>
+      <div class="item">
+        <p>备注：</p>
+        <div style="height: auto;display: flex;align-items: center">
+          <textarea
+            maxlength="-1"
+            adjust-position
+            auto-height
+            placeholder="请输入备注"
+            v-model="beizhu">
+          </textarea>
+        </div>
+      </div>
     </div>
-
-    <!--时间段-->
-    <v-popup
-      :show="is_popup"
-      position="bottom"
-      @close="closePopup"
-    >
-      <!--<v-picker-->
-      <!--show-toolbar-->
-      <!--title="选择时间段"-->
-      <!--:columns="columns"-->
-      <!--@cancel="closeDayTime"-->
-      <!--@confirm="getDayTime">-->
-      <!--</v-picker>-->
-
-
-    </v-popup>
 
   </section>
 </template>
@@ -51,25 +118,52 @@
     components: {},
     data() {
       return {
-        index: 0,
-        array: ["美国", "中国", "巴西", "日本"],
-        is_popup: false,
-        columns: ["上午", "中午", "下午", "晚上"]
+        sex: 0,
+        a0: ["请选择性别", "男", "女"],
+        time: 0,
+        a1: ["请选择时间段", "上午", "中午", "下午", "晚上"],
+        eat: 0,
+        a2: ["请选择用餐情况", "一餐", "二餐", "三餐", "没进食"],
+        disease: 0,
+        a3: ["请选择身体症状", "舒适", "疼痛", "眩晕", "腹胀", "乏力", "咳嗽"],
+        moody: 0,
+        a4: ["请选择情绪情况", "无感", "开心", "沮丧", "焦虑", "正常"],
+        to: 0,
+        a5: ["请选择生理期时间", "进行中", "结束", "马上", "无"],
+        sleep: 0,
+        a6: ["请选择睡眠情况", "良好", "没睡醒", "还可以", "困"],
+        duanlian: 0,
+        a7: ["请选择健身情况", "1次", "2次", "3次", "4次", "5次", "6次", "7次"],
+        shenggao: "",
+        tizhong: "",
+        mubiao: ""
       };
     },
     methods: {
-      bindPickerChange: function(e) {
-        console.log("picker发送选择改变，携带值为", e);
+      selectSex: function(e) {
+        this.sex = e.mp.detail.value;
+        console.log(this.sex);
       },
-      closePopup() {
-        this.is_popup = false;
+      selectTime: function(e) {
+        this.time = e.mp.detail.value;
       },
-      getDayTime(event) {
-        console.log(event.mp.detail);
-        this.is_popup = false;
+      selectEat: function(e) {
+        this.eat = e.mp.detail.value;
       },
-      closeDayTime() {
-        this.is_popup = false;
+      selectDisease: function(e) {
+        this.disease = e.mp.detail.value;
+      },
+      selectMoody: function(e) {
+        this.moody = e.mp.detail.value;
+      },
+      selectTo: function(e) {
+        this.to = e.mp.detail.value;
+      },
+      selectSleep: function(e) {
+        this.sleep = e.mp.detail.value;
+      },
+      selectDuanLian: function(e) {
+        this.duanlian = e.mp.detail.value;
       }
     }
   };
@@ -128,14 +222,15 @@
         div {
           height: 40px;
 
-          input {
+          input, textarea {
             width: 100%;
             height: 100%;
             box-sizing: border-box;
             padding-left: 10px;
+            color: #495060;
           }
 
-          p {
+          picker, p {
             width: 100%;
             height: 100%;
             color: #495060;
