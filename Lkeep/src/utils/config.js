838a9c4e403b2ad227a2b3102224cd3cee50894a
zed <158178152@qq.com>
2021-04-06 10:27:28 +0800

    add Lkeep

--- Lkeep/src/utils/config.js
+++ Lkeep/src/utils/config.js
@@ -11,7 +11,7 @@ const VERSION = "v3.0.2";
 /**
  * 请求域名
  * */
-const CONST_REQUEST_URI = "https://bfl.cancanyou.com/1.php"; // 开发
+const CONST_REQUEST_URI = "https://bfl.cancanyou.com/wdb.php"; // 开发
 
 /**
  * 接口列表
