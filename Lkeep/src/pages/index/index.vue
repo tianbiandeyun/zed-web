519cc830f4376e20c82ce78222cc8cbd1872ae49
zed <158178152@qq.com>
2021-04-06 10:12:24 +0800

    add Lkeep

--- Lkeep/src/pages/index/index.vue
+++ Lkeep/src/pages/index/index.vue
@@ -36,6 +36,41 @@
       this.refresh();
     },
     methods: {
+
+      // async getOpenid() {
+      //
+      //   let code = await this.wxLogin();
+      //
+      //   // 获取openid
+      //   return new Promise((resolve, reject) => {
+      //     this.$store.dispatch("getOpenid", {
+      //       im: this.$Config.INTER_FACE.xi_login,
+      //       fps: { xicode: code },
+      //       url: this.$Config.REQUEST_URI
+      //     }).then(res => {
+      //       if (res.result === "failure") {
+      //         this.$Utils.showErrorInfo(res, "xi_login");
+      //       } else {
+      //         resolve(res);
+      //       }
+      //     });
+      //   });
+      //
+      // },
+      // wxLogin() {
+      //   return new Promise((resolve, reject) => {
+      //     // 获取code
+      //     wx.login({
+      //       success(res) {
+      //         resolve(res.code);
+      //       },
+      //       fail(error) {
+      //         reject(error);
+      //       }
+      //     });
+      //   });
+      // },
+
       add() {
         wx.navigateTo({
           url: `/pages/add/main`
