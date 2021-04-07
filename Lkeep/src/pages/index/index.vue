a81745f31406074bbbb31b68e11fd677eb12ebc0
zed <158178152@qq.com>
2021-04-07 09:54:38 +0800

    add Lkeep

--- Lkeep/src/pages/index/index.vue
+++ Lkeep/src/pages/index/index.vue
@@ -17,148 +17,157 @@
 </template>
 
 <script>
-import ZedRecordItem from "../../components/ZedRecordItem";
-import echart from "../../components/ZedEchart";
-import { mapGetters } from "vuex";
+  import ZedRecordItem from "../../components/ZedRecordItem";
+  import echart from "../../components/ZedEchart";
+  import {
+    mapGetters
+  } from "vuex";
 
-export default {
-  components: { ZedRecordItem, echart },
-  data() {
-    return {
-      list: [],
-    };
-  },
-  async mounted() {
-    await this.getOpenid();
-    this.refresh();
-  },
-  onTabItemTap(res) {
-    this.refresh();
-  },
-  methods: {
-    getOpenid() {
-      let that = this;
-      return new Promise((resolve, reject) => {
-        // 获取code
-        wx.login({
-          success(res) {
-            that.$store
-              .dispatch("getOpenid", {
-                im: "getOpenid",
-                fps: { code: res.code },
-                url: that.$Config.CONST_REQUEST_URI,
-              })
-              .then((res) => {
-                resolve(res);
-              });
-          },
-          fail(error) {
-            reject(error);
-          },
-        });
-      });
+  export default {
+    components: {
+      ZedRecordItem,
+      echart
     },
-    add() {
-      wx.navigateTo({
-        url: `/pages/add/main`,
-      });
+    data() {
+      return {
+        list: [],
+      };
     },
-    async refresh() {
-      this.$Utils.showWaiting();
-
-      await this.$store
-        .dispatch("fetch", {
-          im: "getProjectList",
-          fps: {
-            page: "shouye",
-            openid: this.openidInfo.data.openid,
-          },
-          url: this.$Config.CONST_REQUEST_URI,
-        })
-        .then((res) => {
-          let result = res.data;
-          let new_data = [];
-          let _o = {};
-          result.forEach((item) => {
-            let _m = new Date(item.riqi).getMonth() + 1;
-            if (undefined === _o[_m]) {
-              _o[_m] = [];
-            }
-            _o[_m].push(item);
+    async mounted() {
+      await this.getOpenid();
+      this.refresh();
+    },
+    onTabItemTap(res) {
+      this.refresh();
+    },
+    methods: {
+      getOpenid() {
+        let that = this;
+        return new Promise((resolve, reject) => {
+          // 获取code
+          wx.login({
+            success(res) {
+              that.$store
+                .dispatch("getOpenid", {
+                  im: "getOpenid",
+                  fps: {
+                    code: res.code
+                  },
+                  url: that.$Config.CONST_REQUEST_URI,
+                })
+                .then((res) => {
+                  resolve(res);
+                });
+            },
+            fail(error) {
+              reject(error);
+            },
           });
+        });
+      },
+      add() {
+        wx.navigateTo({
+          url: `/pages/add/main`,
+        });
+      },
+      async refresh() {
+        this.$Utils.showWaiting();
 
-          Object.keys(_o).forEach((item, index) => {
-            new_data.push({
-              month: item,
-              list: Object.values(_o)[index],
+        await this.$store
+          .dispatch("fetch", {
+            im: "getProjectList",
+            fps: {
+              page: "shouye",
+              openid: this.openidInfo.data.openid,
+            },
+            url: this.$Config.CONST_REQUEST_URI,
+          })
+          .then((res) => {
+            let result = res.data;
+            let new_data = [];
+            let _o = {};
+            result.forEach((item) => {
+              let _m = new Date(item.riqi).getMonth() + 1;
+              if (undefined === _o[_m]) {
+                _o[_m] = [];
+              }
+              _o[_m].push(item);
             });
-          });
-          this.list = new_data;
-        });
 
-      this.$Utils.closeWaiting();
+            Object.keys(_o).forEach((item, index) => {
+              new_data.push({
+                month: item,
+                list: Object.values(_o)[index],
+              });
+            });
+            this.list = new_data;
+          });
+
+        this.$Utils.closeWaiting();
+      },
     },
-  },
-  computed: {
-    ...mapGetters(["openidInfo"]),
-  },
-};
+    computed: {
+      ...mapGetters(["openidInfo"]),
+    },
+  };
+
 </script>
 
 <style lang="less" scoped>
-.index-container {
-  padding: 10px 20px 80px 20px;
+  .index-container {
+    padding: 10px 20px 80px 20px;
 
-  .record-box {
-    margin-bottom: 10px;
-
-    .title {
-      padding-left: 20px;
-      font-size: 20px;
-      font-weight: bold;
-      color: #1c2438;
-      background-color: #fff;
-      line-height: 2;
+    .record-box {
       margin-bottom: 10px;
-    }
 
-    .item {
-      margin-bottom: 20px;
+      .title {
+        padding-left: 20px;
+        font-size: 20px;
+        font-weight: bold;
+        color: #1c2438;
+        background-color: #fff;
+        line-height: 2;
+        margin-bottom: 10px;
+      }
+
+      .item {
+        margin-bottom: 20px;
+
+        &:last-child {
+          margin-bottom: 0;
+        }
+      }
 
       &:last-child {
         margin-bottom: 0;
       }
     }
 
-    &:last-child {
-      margin-bottom: 0;
+    .add-box {
+      -webkit-border-radius: 4px;
+      -moz-border-radius: 4px;
+      border-radius: 4px;
+      -moz-box-shadow: 0 0 10px #dddee1;
+      -webkit-box-shadow: 0 0 10px #dddee1;
+      box-shadow: 0 0 10px #dddee1;
+      position: fixed;
+      left: 50%;
+      bottom: 10px;
+      margin-left: -150px;
+      width: 300px;
+      z-index: 999;
+      background-color: #fff;
+      display: grid;
+      align-items: center;
+      justify-content: center;
+      padding: 5px 0;
+
+      p {
+        font-size: 10px;
+        line-height: 2;
+        color: #495060;
+      }
     }
   }
 
-  .add-box {
-    -webkit-border-radius: 4px;
-    -moz-border-radius: 4px;
-    border-radius: 4px;
-    -moz-box-shadow: 0 0 10px #dddee1;
-    -webkit-box-shadow: 0 0 10px #dddee1;
-    box-shadow: 0 0 10px #dddee1;
-    position: fixed;
-    left: 50%;
-    bottom: 10px;
-    margin-left: -150px;
-    width: 300px;
-    z-index: 999;
-    background-color: #fff;
-    display: grid;
-    align-items: center;
-    justify-content: center;
-    padding: 5px 0;
-
-    p {
-      font-size: 10px;
-      line-height: 2;
-      color: #495060;
-    }
-  }
-}
 </style>
