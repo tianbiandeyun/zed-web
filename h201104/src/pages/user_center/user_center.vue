20f3cf2833e6922f149e63f51b3c3e3bde33b915
zed <158178152@qq.com>
2021-04-07 10:39:23 +0800

    mid 201104

--- h201104/src/pages/user_center/user_center.vue
+++ h201104/src/pages/user_center/user_center.vue
@@ -65,7 +65,7 @@
 
     </div>
 
-    <!--<button @click="goReply">留言</button>-->
+    <button @click="goReply">留言</button>
 
     <div class="user-e">
       <h1>形象照片</h1>
@@ -108,7 +108,6 @@
       }
 
       await this.refreshUserCenter();
-
       this.$Utils.closeWaiting();
 
     },
