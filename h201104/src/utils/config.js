73dc990f09883ab18178909d5a32f4b568606051
zed <158178152@qq.com>
2021-04-15 17:23:51 +0800

    mid 201104

--- h201104/src/utils/config.js
+++ h201104/src/utils/config.js
@@ -23,6 +23,7 @@ const REQUEST_URI = `${CONST_REQUEST_URI}?r=interface/api&ic=${CONST_APP_IC}`;
  * 接口列表
  * */
 const INTER_FACE = {
+  get_occupation_list: 'get_occupation_list',
   get_unread_message: 'get_unread_message',
   read_message: 'read_message',
   set_initiate_a_session: 'set_initiate_a_session',
