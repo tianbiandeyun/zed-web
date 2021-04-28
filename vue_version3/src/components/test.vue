062bf7135eb4efa36d37fccfa5a56542be1599ee
zed <158178152@qq.com>
2021-04-28 11:27:08 +0800

    mid 201104

---
+++ vue_version3/src/components/test.vue
@@ -0,0 +1,16 @@
+<template>
+    <section class="text-container">
+        test
+    </section>
+</template>
+<script>
+    export default {
+        name: 'test',
+        setup(props) {
+            console.log(props);
+        }
+    }
+</script>
+<style lang="less">
+
+</style>
--- vue_version3/src/views/Home.vue
+++ vue_version3/src/views/Home.vue
@@ -1,14 +1,39 @@
 <template>
-  <section class="home">
-    home
+  <section class="home-contaienr">
+    <button @click="countAdd">add</button>
+    <p>{{count}}</p>
+    <test></test>
   </section>
 </template>
-
 <script>
+  import test from '@/components/test'
+  import {
+    reactive,
+    toRefs
+  } from 'vue'
   export default {
-    name: 'Home',
+    name: 'home',
     components: {
+      test
+    },
+    setup(props, context) {
 
+      const data = reactive({
+        count: 99,
+      })
+
+      const countAdd = () => {
+        data.count++;
+        console.log(context);
+      }
+
+      return {
+        ...toRefs(data),
+        countAdd
+      }
     }
   }
 </script>
+<style lang="less">
+
+</style>
