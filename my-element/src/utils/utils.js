83a2ad2cb659a492a97fc7646fca78185db42f44
zed <158178152@qq.com>
2021-06-03 21:58:44 +0800

    mid my-element

--- my-element/src/utils/utils.js
+++ my-element/src/utils/utils.js
@@ -1,3 +1,5 @@
+import Mock from "mockjs";
+
 export const setSessionStorage = (...res) => {
     window.sessionStorage.setItem(res[0], res[1]);
 }
@@ -8,4 +10,21 @@ export const getSessionStorage = (res) => {
 
 export const removeSessionStorage = (res) => {
     window.sessionStorage.removeItem(res);
+}
+
+export const generateTable = () => {
+    return Mock.mock({
+        "table|1-10": [{
+            "id|+1": 1,
+            username: "@cname",
+            mobile: /^1[0-9]{10}$/,
+            "type|1-2": 1,
+            email: "@email()",
+            create_time: '@date("yyyy-MM-dd")',
+            "user_state|1": true,
+            role: function () {
+                return this.user_state ? "超级管理员" : "普通用户";
+            },
+        }]
+    });
 }
