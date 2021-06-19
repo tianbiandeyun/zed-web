f92de9ea14c130f4dab8aa55b5d914acd600b77f
zed <158178152@qq.com>
2021-06-20 00:38:51 +0800

    reset baset

--- vue-element-admin/src/views/permission/page.vue
+++ vue-element-admin/src/views/permission/page.vue
@@ -1,19 +1,9 @@
 <template>
-  <div class="app-container">
-    <switch-roles @change="handleRolesChange" />
-  </div>
+  <div class="page-container"></div>
 </template>
 
 <script>
-import SwitchRoles from './components/SwitchRoles'
-
 export default {
-  name: 'PagePermission',
-  components: { SwitchRoles },
-  methods: {
-    handleRolesChange() {
-      this.$router.push({ path: '/permission/index?' + +new Date() })
-    }
-  }
-}
+  name: "PagePermission",
+};
 </script>
