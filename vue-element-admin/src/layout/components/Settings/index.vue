ed39605db7b85e9f969bafac875107afc903aefa
zed <158178152@qq.com>
2021-06-20 09:46:16 +0800

    mid,6-20

--- vue-element-admin/src/layout/components/Settings/index.vue
+++ vue-element-admin/src/layout/components/Settings/index.vue
@@ -1,86 +1,33 @@
 <template>
   <div class="drawer-container">
     <div>
-      <h3 class="drawer-title">Page style setting</h3>
+      <h3 class="drawer-title">页面抽屉工具栏</h3>
 
       <div class="drawer-item">
         <span>Theme Color</span>
-        <theme-picker
-          style="float: right; height: 26px; margin: -3px 8px 0 0"
-          @change="themeChange"
-        />
       </div>
 
       <div class="drawer-item">
         <span>Open Tags-View</span>
-        <el-switch v-model="tagsView" class="drawer-switch" />
       </div>
 
       <div class="drawer-item">
         <span>Fixed Header</span>
-        <el-switch v-model="fixedHeader" class="drawer-switch" />
       </div>
 
       <div class="drawer-item">
         <span>Sidebar Logo</span>
-        <el-switch v-model="sidebarLogo" class="drawer-switch" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import ThemePicker from '@/components/ThemePicker'
-
 export default {
-  components: { ThemePicker },
   data() {
-    return {}
+    return {};
   },
-  computed: {
-    fixedHeader: {
-      get() {
-        return this.$store.state.settings.fixedHeader
-      },
-      set(val) {
-        this.$store.dispatch('settings/changeSetting', {
-          key: 'fixedHeader',
-          value: val
-        })
-      }
-    },
-    tagsView: {
-      get() {
-        return this.$store.state.settings.tagsView
-      },
-      set(val) {
-        this.$store.dispatch('settings/changeSetting', {
-          key: 'tagsView',
-          value: val
-        })
-      }
-    },
-    sidebarLogo: {
-      get() {
-        return this.$store.state.settings.sidebarLogo
-      },
-      set(val) {
-        this.$store.dispatch('settings/changeSetting', {
-          key: 'sidebarLogo',
-          value: val
-        })
-      }
-    }
-  },
-  methods: {
-    themeChange(val) {
-      this.$store.dispatch('settings/changeSetting', {
-        key: 'theme',
-        value: val
-      })
-    }
-  }
-}
+};
 </script>
 
 <style lang="scss" scoped>
@@ -101,10 +48,6 @@ export default {
     color: rgba(0, 0, 0, 0.65);
     font-size: 14px;
     padding: 12px 0;
-  }
-
-  .drawer-switch {
-    float: right;
   }
 }
 </style>
